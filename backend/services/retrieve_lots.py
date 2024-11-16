d57e9e11b7f2abdf638c3d2f44dddb7777603ac2
jeremyquek <jeremyqjs@gmail.com>
2024-11-16 22:58:49 +0800

    ok try AGAIN

--- backend/services/retrieve_lots.py
+++ backend/services/retrieve_lots.py
@@ -10,15 +10,6 @@ HDB_URL=os.getenv("HDB_LOTS_URL")
 LTA_URL=os.getenv("LTA_LOTS_URL")
 ACCESS_KEY=os.getenv("LTA_ACCESS_KEY")
 
-# def retrieve_HDB_lots():
-#     try:
-#         response = requests.get(HDB_URL)
-#         response.raise_for_status()
-#         data = response.json()
-#         return data["items"][0]['carpark_data'] #dictionary
-#     except requests.exceptions.RequestException as e:
-#         return None
-
 def search_by_id_HDB(id, lots_data):
     for lot in lots_data:
         if lot["carpark_number"]==id:
@@ -30,20 +21,6 @@ def update_result_with_HDB_lots(carpark_data):
         res=search_by_id_HDB(carpark["carpark_id"], lots_data)
         carpark.update(res[0]) if res else carpark.update({"lot_type":None})
     return carpark_data
-
-# def retrieve_LTA_lots():
-#     headers = {
-#         'AccountKey': ACCESS_KEY,
-#         'Accept': 'application/json'
-#     }
-#     try:
-#         response = requests.get(
-#             LTA_URL,
-#             headers=headers
-#         )
-#         return [data for data in response.json()['value'] if data['Agency']!="HDB"] # dictionary
-#     except requests.exceptions.RequestException as e:
-#         return None
 
 def search_by_id_LTA(id, lots_data):
     for lot in lots_data:
