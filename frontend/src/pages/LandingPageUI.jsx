2076cc8a514ddffc555c5cb9e63f388e3dbb23dd
jeremyquek <jeremyqjs@gmail.com>
2024-11-21 23:05:11 +0800

    please

--- frontend/src/pages/LandingPageUI.jsx
+++ frontend/src/pages/LandingPageUI.jsx
@@ -37,7 +37,7 @@ function LandingPage() {
           style={{
             position: "absolute",
             top: "25%",
-            left: "40%",
+            left: "25%",
             transform: "translate(-50%, -50%)",
             textAlign: "center",
           }}
